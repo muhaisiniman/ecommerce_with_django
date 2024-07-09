0af8ffc0aeb844f217aae3d87151960a6e5b0634
muhaisiniman <m4muhaisin@gmail.com>
2024-07-09 18:47:29 +0300

    added css files

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
 
+app_name = 'ecommerce'
+
 urlpatterns = [
-    path ('product_list', views.product_list, name='product_list'),
+    path('', views.home, name='home'),
+    path('products', views.product_list, name='product_list'),
+    path('products/<int:pk>/', views.product_detail, name='product_detail'),
+    path('customers/', views.customer_list, name='customer_list'),
+    path('customers/<int:pk>', views.customer_detail, name='customer_detail'),
 
 ]
--- my_project/urls.py
+++ my_project/urls.py
@@ -19,6 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('ecommerce.urls')),
+    path('ecommerce/',include('ecommerce.urls')),
     
 ]
